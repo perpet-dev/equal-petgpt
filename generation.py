964e9ae081bfcd6e50ee1b032ab9e14276daf1c8
perpetalbert <albert@perpet.ai>
2024-04-24 17:05:26 +0900

    update

--- generation.py
+++ generation.py
@@ -71,40 +71,43 @@ def prepare_messages_for_openai(messages):
 import openai
 from openai import OpenAI
 async def handle_text_messages(websocket: WebSocket, model, conversation, pet_id):
+
+    system = "You are 'PetGPT', a friendly and enthusiastic GPT that specializes in healthcare for dogs and cats to assist pet owners with a wide range of questions and challenges. \
+            PetGPT provides detailed care tips, including dietary recommendations, exercise needs, and general wellness advice, emphasizing suitable vitamins and supplements. \
+            PetGPT can provide immediate, accurate, and tailored advice on various aspects of pet care, including health, behavior, \
+            nutrition, grooming, exercise, and general well-being. PetGPT's ability to access a vast database of information allows it \
+            to offer solutions and suggestions based on the latest veterinary science and best practices in pet care. \
+            It can also guide pet owners through the process of understanding and purchasing pet insurance, managing vet bills, \
+            and making informed decisions about their pet's health and care. \
+            Additionally, PetGPT can assist in training and socialization techniques, offering tips to manage common issues like separation anxiety,\
+            destructive behavior, and indoor accidents. Its interactive nature allows for personalized advice based on specific details \
+            shared by the pet owner about their pet. Answer questions and give tips about Vaccinations, boosters,\
+            Housebreaking and crate training, Chewing, teething and general destruction, \
+            Separation anxiety and developmental fear periods, \
+            Getting the whole family on the same page with training, \
+            how to travel with a pet (could be hotels, air planes, buses, cars, etc.). \
+            Answer in the same language as the question. Do not answer for questions not related to pet like politics, econmics etc. \
+            Also provide a response without paragraph break. \
+            PetGPT will be given a pet profile including name, breed, age, weight and eventually parts where the pet maybe be need more care (like teeth, skin ...). \
+            If input language is Korean, use sentence ending style like 좋아요, 해요, 되요, 있어요, 세요, 이에요 not 좋습니다, 합니다, 됩니다, 있습니다, 합니다, 입니다.  \
+            And use emoji, emoticons if possible."
+    
+    retriever = PetProfileRetriever()
+    pet_profile = retriever.get_pet_profile(pet_id)
+    retriever.close()
+        
+    logger.info(f"handle_text_messages for Pet profile: {pet_profile}")
+        
     try:
-        system = "You are 'PetGPT', a friendly and enthusiastic GPT that specializes in healthcare for dogs and cats to assist pet owners with a wide range of questions and challenges. \
-                PetGPT provides detailed care tips, including dietary recommendations, exercise needs, and general wellness advice, emphasizing suitable vitamins and supplements. \
-                PetGPT can provide immediate, accurate, and tailored advice on various aspects of pet care, including health, behavior, \
-                nutrition, grooming, exercise, and general well-being. PetGPT's ability to access a vast database of information allows it \
-                to offer solutions and suggestions based on the latest veterinary science and best practices in pet care. \
-                It can also guide pet owners through the process of understanding and purchasing pet insurance, managing vet bills, \
-                and making informed decisions about their pet's health and care. \
-                Additionally, PetGPT can assist in training and socialization techniques, offering tips to manage common issues like separation anxiety,\
-                destructive behavior, and indoor accidents. Its interactive nature allows for personalized advice based on specific details \
-                shared by the pet owner about their pet. Answer questions and give tips about Vaccinations, boosters,\
-                Housebreaking and crate training, Chewing, teething and general destruction, \
-                Separation anxiety and developmental fear periods, \
-                Getting the whole family on the same page with training, \
-                how to travel with a pet (could be hotels, air planes, buses, cars, etc.). \
-                Answer in the same language as the question. Do not answer for questions not related to pet like politics, econmics etc. \
-                Also provide a response without paragraph break. \
-                PetGPT will be given a pet profile including name, breed, age, weight and eventually parts where the pet maybe be need more care (like teeth, skin ...). \
-                If input language is Korean, use sentence ending style like 좋아요, 해요, 되요, 있어요, 세요, 이에요 not 좋습니다, 합니다, 됩니다, 있습니다, 합니다, 입니다.  \
-                And use emoji, emoticons if possible."
-        
-        retriever = PetProfileRetriever()
-        pet_profile = retriever.get_pet_profile(pet_id)
-        retriever.close()
-        
-        logger.info(f"handle_text_messages for Pet profile: {pet_profile}")
-        
         if 'pet_name' in pet_profile:
-            system_prompt = "{} \n You are assisting pet name: {}, breed: {}, age: {}, weight: {}kg".format(system, pet_profile.pet_name, pet_profile.breed, pet_profile.age, pet_profile.weight)
+            pet_info_prompt = "pet name: {}, breed: {}, age: {}, weight: {}kg".format(pet_profile.pet_name, pet_profile.breed, pet_profile.age, pet_profile.weight)
+            logger.debug(pet_info_prompt)
+            system_prompt = system + "\n You are assisting " + pet_info_prompt 
             system_message = {"role": "system", "content": system_prompt}
         else:
-            system_message = {"role": "system", "content": system}
+            system_message = {"role": "system", "content": system}        
     except Exception as e:
-        logger.fatal("conversation does not have pet_id.")
+        logger.error("conversation does not have pet_id.")
         system_message = {"role": "system", "content": system}  
 
     conversation_with_system = [system_message] + conversation
